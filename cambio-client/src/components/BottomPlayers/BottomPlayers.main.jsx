afebd565e6028cf07f00cb17282925dc11694599
pirainogi <pirainogi@gmail.com>
2020-03-28 13:31:24 -0400

    bottom player area

---
+++ cambio-client/src/components/BottomPlayers/BottomPlayers.main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BottomPlayers = (props) => {
+
+  return (
+    <div className="bottom-players">
+      <p>one day i will be the individual player components and i need logic later on how to render different numbers of players</p>
+      <p>current user goes here</p>
+    </div>
+  )
+
+}
+
+export default BottomPlayers;
