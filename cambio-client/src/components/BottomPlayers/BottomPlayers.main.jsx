b5bb554ef7f483cf3e1f3afc67cd957b90737e69
pirainogi <pirainogi@gmail.com>
2020-03-28 15:38:25 -0400

    player 2 6

--- cambio-client/src/components/BottomPlayers/BottomPlayers.main.jsx
+++ cambio-client/src/components/BottomPlayers/BottomPlayers.main.jsx
@@ -6,8 +6,9 @@ const BottomPlayers = () => {
 
   return (
     <div className="bottom-players">
-      <p>one day i will be the individual player components and i need logic later on how to render different numbers of players</p>
+      <div className="player-two">player 2</div>
       <CurrentUser />
+      <div className="player-six">player 6</div>
     </div>
   )
 
