0793780ca70b987922292ef19dafdabfc7ad5728
ihollander <ianholla@gmail.com>
2020-03-28 16:48:25 -0400

    starting game

---
+++ cambio-client/src/store/player/reducer.js
@@ -0,0 +1,31 @@
+import {
+    START_GAME,
+    STARTING_GAME
+} from './types'
+
+const initialState = {
+    players: null,
+    loading: true
+}
+
+const reducerActions = {
+    [START_GAME](state, action) {
+        return {
+            ...state,
+            players: action.payload,
+            loading: false
+        }
+    },
+    [STARTING_GAME](state) {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+}
+
+const playersReducer = (state = initialState, action) => {
+    return reducerActions[action.type] ? reducerActions[action.type](state, action) : state
+}
+
+export default playersReducer
