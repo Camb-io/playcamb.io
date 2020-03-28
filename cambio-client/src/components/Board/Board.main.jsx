85b15efebb6be0190c5a3089396b1ccb90ae0def
ihollander <ianholla@gmail.com>
2020-03-28 18:18:56 -0400

    call start game from table

--- cambio-client/src/components/Board/Board.main.jsx
+++ cambio-client/src/components/Board/Board.main.jsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { TopPlayers } from './'
 import { Decks } from '../Decks'
 import { BottomPlayers } from './'
 import * as boardSelectors from '../../store/board/selectors'
-import * as boardActions from '../../store/board/actions'
 
 const Board = (props) => {
-  const dispatch = useDispatch()
   const players = useSelector(boardSelectors.getPlayers)
   
-  useEffect(() => {
-    dispatch(boardActions.startGame())
-  }, [])
-
   console.log(players)
 
   return (
--- cambio-client/src/components/Table/Table.main.jsx
+++ cambio-client/src/components/Table/Table.main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Board } from '../Board'
 import { Chat } from '../Chat'
+import * as boardActions from '../../store/board/actions'
 
 const Table = (props) => {
+  const { slug } = props.match.params
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(boardActions.startGame(slug))
+  }, [])
+
   return (
     <main className="table-container">
       <Board />
-      <Chat/>
+      <Chat />
     </main>
   )
 }
