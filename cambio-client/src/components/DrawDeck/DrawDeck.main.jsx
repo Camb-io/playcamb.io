cc9de86bb649b944c5aa093cdb1b4537f34b05ae
pirainogi <pirainogi@gmail.com>
2020-03-28 15:10:13 -0400

    decks

---
+++ cambio-client/src/components/DrawDeck/DrawDeck.main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DrawDeck = () => {
+
+  return (
+    <div className="drawdeck-area">
+      <p>i am the deck you can draw from</p>
+    </div>
+  )
+
+}
+
+export default DrawDeck;
