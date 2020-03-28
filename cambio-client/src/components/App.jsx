87b07207f5bf8e91e4b018103b2b2dee1d96b8a5
ihollander <ianholla@gmail.com>
2020-03-28 13:25:09 -0400

    initial hand layout

--- cambio-client/src/components/App.jsx
+++ cambio-client/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Board } from './Board'
+import { Hand } from './Hand'
 
 function App() {
   return (
     <div className="App">
-      <Board />
+      <Hand />
+      <Hand />
     </div>
   );
 }
---
+++ cambio-client/src/components/Hand/Hand.main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Hand = () => {
+  // this will likely come from props from the Board, hard coding for now
+  const hand = {
+    cards: ["AS", "10D", "JH", "2C"],
+    penalty: ["3S", "5C"]
+  }
+
+  const renderCards = cards =>
+    // this should render a Card component 
+    cards.map(card => (
+      <div className="card" key={card}>
+        {card}
+      </div>
+    ))
+
+  return (
+    <div className="hand">
+      <div className="cards">
+        {renderCards(hand.cards)}
+      </div>
+      <div className="penalty">
+        {renderCards(hand.penalty)}
+      </div>
+    </div>
+  )
+}
+
+export default Hand
