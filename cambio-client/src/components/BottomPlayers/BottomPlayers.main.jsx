f603dca1ef30eac39d02e531080cc18f1b450c52
pirainogi <pirainogi@gmail.com>
2020-03-28 15:55:42 -0400

    added space btw hands

--- cambio-client/src/components/BottomPlayers/BottomPlayers.main.jsx
+++ cambio-client/src/components/BottomPlayers/BottomPlayers.main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { CurrentPlayer } from '../CurrentPlayer'
+import { Hand } from '../Hand'
 
 
 const BottomPlayers = () => {
 
   return (
     <div className="bottom-players">
-      <div className="player-two">player 2</div>
+      <div className="player-two">player 2
+        <Hand/>
+      </div>
       <CurrentPlayer />
-      <div className="player-six">player 6</div>
+      <div className="player-six">player 6
+        <Hand/>
+      </div>
     </div>
   )
 
