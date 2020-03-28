f4ed363d03b1c73cf4c646574d82a0a624c23f70
sbal13 <sbal13@gmail.com>
2020-03-28 16:58:20 -0400

    Revert "Revert "C 01""

---
+++ cambio-client/src/components/Decks/DiscardDeck.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DiscardDeck = () => {
+
+  return (
+    <div className="discarddeck-area">
+      <p>i am the discard deck</p>
+    </div>
+  )
+
+}
+
+export default DiscardDeck;
