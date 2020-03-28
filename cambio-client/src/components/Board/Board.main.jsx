b8ab1648e81e6904003477d61d452ccf65fb3e39
sbal13 <sbal13@gmail.com>
2020-03-28 16:51:42 -0400

    Revert "C 01"

--- cambio-client/src/components/Board/Board.main.jsx
+++ cambio-client/src/components/Board/Board.main.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as boardSelectors from '../../store/board/selectors'
 import * as boardActions from '../../store/board/actions'
-import { TopPlayers } from './'
-import { Decks } from '../Decks'
-import { BottomPlayers } from './'
-
 
 const Board = (props) => {
+  const dispatch = useDispatch()
+  const beef = useSelector(boardSelectors.getBeef)
+  
+  const handleClick = () => {
+    dispatch(boardActions.setBeef("Delish"))
+  }
+
+
   return (
-    <div className="full-board">
-      <TopPlayers/>
-      <Decks/>
-      <BottomPlayers/>
+    <div>
+      {beef}
+      <button className="board-button" onClick={handleClick} >click me</button>
     </div>
   )
 }
