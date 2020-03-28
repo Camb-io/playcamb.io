4827a9671aa2646e311c7c4f12ad84a2533ad1e7
Andrew Sproul <andrewsproul@andrews-mbp.home>
2020-03-28 15:28:22 -0400

    updating gitignore + table class, still imcomplete

--- cambio-server/models/CambioTable.py
+++ cambio-server/models/CambioTable.py
@@ -2,5 +2,11 @@ class CambioTable:
     id: str = ""
     players: list = []
 
-    def __init__(self, table_id):
+    def __init__(self, table_id, table_admin, player_id, url_suffix):
+        self.active_game = True
         self.id = table_id
+        self.admin_id = table_admin
+        self.active_player_id = player_id
+        self.table_url_suffix = url_suffix
+        self.sockets = '???'
+        self.scores = {}
