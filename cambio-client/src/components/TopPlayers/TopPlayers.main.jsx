0ce3eb60112f1a02e0526af85a2e545e1117e7e4
pirainogi <pirainogi@gmail.com>
2020-03-28 15:55:42 -0400

    added space btw hands

--- cambio-client/src/components/TopPlayers/TopPlayers.main.jsx
+++ cambio-client/src/components/TopPlayers/TopPlayers.main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Hand } from '../Hand';
 
 const TopPlayers = () => {
 
   return (
     <div className="top-players">
-      <div className="player-three">player 3</div>
-      <div className="player-four">player 4</div>
-      <div className="player-five">player 5</div>
+      <div className="player-three">player 3
+        <Hand/>
+      </div>
+      <div className="player-four">player 4
+        <Hand/>
+      </div>
+      <div className="player-five">player 5
+        <Hand/>
+      </div>
     </div>
   )
 
