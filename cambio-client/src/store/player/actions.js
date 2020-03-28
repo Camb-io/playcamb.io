0ff96fd9fb7d5d10c5fc00ff19bc6630a76b803c
ihollander <ianholla@gmail.com>
2020-03-28 16:48:25 -0400

    starting game

---
+++ cambio-client/src/store/player/actions.js
@@ -0,0 +1,18 @@
+import { action } from 'typesafe-actions'
+import { mockFetch } from '../../mocks/fetch'
+
+import {
+    START_GAME,
+    STARTING_GAME
+} from './types'
+
+export const startGame = () => {
+    return dispatch => {
+        dispatch(action(STARTING_GAME))
+
+        mockFetch("/deal")
+            .then(players => {
+                dispatch(action(START_GAME, players))
+            })
+    }
+}
---
+++ cambio-client/src/store/player/types.js
@@ -0,0 +1,2 @@
+export const START_GAME = "@@board/START_GAME"
+export const STARTING_GAME = "@@board/STARTING_GAME"
