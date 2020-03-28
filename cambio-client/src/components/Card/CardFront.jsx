a8b706ff7734ac763d1b0d1d97f00901ae012170
ihollander <ianholla@gmail.com>
2020-03-28 14:10:50 -0400

    card classes

--- cambio-client/src/components/Card/CardFront.jsx
+++ cambio-client/src/components/Card/CardFront.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const CardFront = ({ card }) => {
+const CardFront = (props) => {
+  const card = {
+    rank: 10,
+    suit: "&hearts;"
+  }
 
   return (
     <div className="card front">
-      
+      <div className="top">
+        <span className="rank">{card.rank}</span>
+        <span className="suit">{card.suit}</span>
+      </div>
+      <div className="middle">
+        <span className="suit">{card.suit}</span>
+      </div>
+      <div className="bottom">
+        <span className="rank">{card.rank}</span>
+        <span className="suit">{card.suit}</span>
+      </div>
     </div>
   )
 }
