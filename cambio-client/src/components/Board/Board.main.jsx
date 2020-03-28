b70e055fbd5d4ec58ae4be58b985950ed3100c9e
pirainogi <pirainogi@gmail.com>
2020-03-28 13:22:29 -0400

    starting grid layout

--- cambio-client/src/components/Board/Board.main.jsx
+++ cambio-client/src/components/Board/Board.main.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import * as boardSelectors from '../../store/board/selectors'
 import * as boardActions from '../../store/board/actions'
 
+
 const Board = (props) => {
   const dispatch = useDispatch()
   const beef = useSelector(boardSelectors.getBeef)
