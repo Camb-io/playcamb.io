c09ec3ff3039c9b09a1971a0dfba1e70ab618338
ihollander <ianholla@gmail.com>
2020-03-28 20:41:40 -0400

    added current player cards

--- cambio-client/src/components/CurrentPlayer/CurrentPlayer.main.jsx
+++ cambio-client/src/components/CurrentPlayer/CurrentPlayer.main.jsx
@@ -2,11 +2,39 @@ import React from 'react';
 import { Hand } from '../Hand'
 import { CurrentPlayerControls } from './'
 
-const CurrentPlayer = () => {
+// test data
+const cards = {
+  topLeft: {
+    rank: "10",
+    suit: "spades",
+    visible: true
+  },
+  topRight: {
+    rank: "J",
+    suit: "hearts",
+    visible: true
+  },
+  bottomLeft: {
+    rank: "2",
+    suit: "clubs",
+    visible: true
+  },
+  bottomRight: {
+    rank: "3",
+    suit: "diamonds",
+    visible: true
+  },
+  penalty1: {
+    rank: "7",
+    suit: "clubs",
+    visible: true
+  }
+}
 
+const CurrentPlayer = () => {
   return (
     <div className="current-player">
-      <Hand />
+      <Hand cards={cards} />
       <CurrentPlayerControls/>
     </div>
   )
